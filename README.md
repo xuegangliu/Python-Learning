dd59e5b7af7c84e96308abfca6a90cae972f4afa
xgliu <lxghoney@163.com>
2016-12-30 14:37:16 +0800

    xml解析demo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Python-Learning
+# 官方网站
+https://www.python.org/
+# 官方文档
+https://docs.python.org/3/
+# 教程学习
+http://www.runoob.com/python3/python3-tutorial.html
+
+
+# xml
+## python对XML的解析方式
+1.SAX (simple API for XML )
+	python 标准库包含SAX解析器，SAX用事件驱动模型，通过在解析XML的过程中触发一个个的事件并调用用户定义的回调函数来处理XML文件。
+	[SAX方式官方解析文档](https://docs.python.org/3/library/xml.sax.html)
+2.DOM(Document Object Model)
+	将XML数据在内存中解析成一个树，通过对树的操作来操作XML。
+	[DOM方式官方解析文档](https://docs.python.org/3/library/xml.dom.html)
---
+++ xml/sax.py
@@ -0,0 +1,74 @@
+# coding=UTF-8
+# 解析XML实例
+# SAX是一种基于事件驱动的API。
+# 利用SAX解析XML文档牵涉到两个部分:1.解析器  2.事件处理器。
+# 解析器负责读取XML文档,并向事件处理器发送事件,如元素开始跟元素结束事件;
+# 而事件处理器则负责对事件作出相应,对传递的XML数据进行处理。
+# --------------------------------------------------------
+# 1、对大型文件进行处理；
+# 2、只需要文件的部分内容，或者只需从文件中得到特定信息。
+# 3、想建立自己的对象模型的时候。
+
+import xml.sax
+
+class MovieHandler( xml.sax.ContentHandler ):
+   def __init__(self):
+      self.CurrentData = ""
+      self.type = ""
+      self.format = ""
+      self.year = ""
+      self.rating = ""
+      self.stars = ""
+      self.description = ""
+
+   # 元素开始调用
+   def startElement(self, tag, attributes):
+      self.CurrentData = tag
+      if tag == "movie":
+         print ("*****Movie*****")
+         title = attributes["title"]
+         print ("Title:", title)
+
+   # 元素结束调用
+   def endElement(self, tag):
+      if self.CurrentData == "type":
+         print ("Type:", self.type)
+      elif self.CurrentData == "format":
+         print ("Format:", self.format)
+      elif self.CurrentData == "year":
+         print ("Year:", self.year)
+      elif self.CurrentData == "rating":
+         print ("Rating:", self.rating)
+      elif self.CurrentData == "stars":
+         print ("Stars:", self.stars)
+      elif self.CurrentData == "description":
+         print ("Description:", self.description)
+      self.CurrentData = ""
+
+   # 读取字符时调用
+   def characters(self, content):
+      if self.CurrentData == "type":
+         self.type = content
+      elif self.CurrentData == "format":
+         self.format = content
+      elif self.CurrentData == "year":
+         self.year = content
+      elif self.CurrentData == "rating":
+         self.rating = content
+      elif self.CurrentData == "stars":
+         self.stars = content
+      elif self.CurrentData == "description":
+         self.description = content
+  
+if ( __name__ == "__main__"):
+   
+   # 创建一个 XMLReader
+   parser = xml.sax.make_parser()
+   # turn off namepsaces
+   parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+   # 重写 ContextHandler
+   Handler = MovieHandler()
+   parser.setContentHandler( Handler )
+   
+   parser.parse("movies.xml")
