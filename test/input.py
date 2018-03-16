f3976fefd8eb2bea0113edf10dfc0974560a7af3
Max <lxghoney@163.com>
2018-03-16 17:41:34 +0800

    test

---
+++ test/input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import sys
+
+print sys.argv
+#a=raw_input("按下 enter 键退出,其它任意键显示...\n")
+#print a
---
+++ test/type.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+#coding=utf-8
+
+a = 1
+str = 'this is string 1'
+list = ['this', 'is', 'list', 2]
+tuple = ('this', 'is', 'tuple', 3)
+dict = {1:'this', 2:'is', 3:'dictionary', 4:4}
+
+print a,type(1)
+print str,type(str)
+print list,type(list)
+print tuple,type(tuple)
+print dict,type(dict)
