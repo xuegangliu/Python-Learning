7426abd6c549227373f291688431ca04e03fcb29
Max <lxghoney@163.com>
2018-03-16 18:18:21 +0800

    test

--- test/type.py
+++ test/type.py
@@ -2,13 +2,19 @@
 #coding=utf-8
 
 a = 1
-str = 'this is string 1'
-list = ['this', 'is', 'list', 2]
-tuple = ('this', 'is', 'tuple', 3)
-dict = {1:'this', 2:'is', 3:'dictionary', 4:4}
+str1 = 'this is string 1'
+list1 = ['this', 'is', 'list', 2]
+tuple1 = ('this', 'is', 'tuple', 3)
+dict1 = {1:'this', 2:'is', 3:'dictionary', 4:4}
 
-print a,type(1)
-print str,type(str)
-print list,type(list)
-print tuple,type(tuple)
-print dict,type(dict)
+print a,type(a)
+print str1,type(str1)
+print list1,type(list1)
+print tuple1,type(tuple1)
+print dict1,type(dict1)
+
+if isinstance(a, int):
+	print 'true'
+else:
+	print 'false'
+print isinstance(str1,str)
