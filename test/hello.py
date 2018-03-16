54791dd794017ff57be109b980161a76d1a70c35
Max <lxghoney@163.com>
2018-03-16 16:39:25 +0800

    test

---
+++ test/hello.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#coding=utf-8
+
+print '你好'
+print 'Hello World!!'
+
+if True:
+	print 'true'
+	print '1'
+else:
+	print 'false'
+	print '2'
+
+total=1+\
+	2+\
+	3
+print total
+
+arrayss=[1,2,
+	3,4,5]
+print arrayss
