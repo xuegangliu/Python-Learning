69d63aff0b0929ed7dc6c979d181c6261bd2dba9
liuxg3 <liuxg3@asiainfo.com>
2018-09-02 10:58:40 +0800

    *args, **kwargs 参数使用

--- src/org/lxg/python3/__init__.py
+++ src/org/lxg/python3/__init__.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 # coding=utf-8
 # author: xuegangliu
+
+
+def foo(*args, **kwargs):
+    '''*args, **kwargs 参数使用'''
+    print('args =', args)
+    print('kwargs = ', kwargs)
+    print('-----------------------')
+
+
+if __name__ == '__main__':
+    foo(1, 2, 3, 4)
+    foo(a=1, b=2, c=3)
+    foo(1,2,3,4, a=1, b=2, c=3)
+    foo('a', 1, None, a=1, b='2', c=3)
