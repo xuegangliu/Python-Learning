ccdf94292a15dbf4dd093187ffa0eae62d457026
Max <lxghoney@163.com>
2018-10-18 14:30:36 +0800

    添加帮助提示

--- src/org/lxg/test/fileModule/displayFileSize.py
+++ src/org/lxg/test/fileModule/displayFileSize.py
@@ -9,6 +9,8 @@
 """
 import os
 import time
+import argparse
+import operator
 
 def TimeStampToTime(timestamp):
     """
@@ -19,7 +21,7 @@ def TimeStampToTime(timestamp):
     timeStruct = time.localtime(timestamp)
     return time.strftime('%Y-%m-%d %H:%M:%S',timeStruct)
 
-def displayDirectory(directory):
+def displayDirectory(directory,sizeSwitch):
     """
     显示目录下所有文件列表及大小
     :param directory:文件目录
@@ -29,10 +31,23 @@ def displayDirectory(directory):
         for filename in filenames:
             filepath = os.path.join(dirpath,filename)
             if os.path.isfile(filepath):
-                print('文件名:'+filename,'文件大小:'+str(os.path.getsize(filepath))+'KB',
-                      '访问时间:'+TimeStampToTime(os.path.getatime(filepath)),
-                      '创建时间:'+TimeStampToTime(os.path.getctime(filepath)),
-                      '修改时间:'+TimeStampToTime(os.path.getmtime(filepath)))
+                if operator.eq(sizeSwitch,"KB"):
+                    print('文件名:'+filename,'文件大小:'+str(os.path.getsize(filepath))+'KB',
+                          '访问时间:'+TimeStampToTime(os.path.getatime(filepath)),
+                          '创建时间:'+TimeStampToTime(os.path.getctime(filepath)),
+                          '修改时间:'+TimeStampToTime(os.path.getmtime(filepath)))
+                if operator.eq(sizeSwitch,'MB'):
+                    print('文件名:'+filename,'文件大小:'+str(round(os.path.getsize(filepath)/float(1024*1024),2))+'MB',
+                          '访问时间:'+TimeStampToTime(os.path.getatime(filepath)),
+                          '创建时间:'+TimeStampToTime(os.path.getctime(filepath)),
+                          '修改时间:'+TimeStampToTime(os.path.getmtime(filepath)))
+def get_parser():
+    parser = argparse.ArgumentParser(description='显示目录下所有文件列表')
+    parser.add_argument('size', metavar='size', type=str, nargs=1, help='按KB|MB显示文件大小')
+    return parser
 
 if __name__ == '__main__':
-    displayDirectory("D:\\test")
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    sizeSwitch = str(args['size'][0]).split('=')[1]
+    displayDirectory("D:\\test",sizeSwitch)
