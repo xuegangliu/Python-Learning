ce23c7400a679e6e63be6ae4a9fe4af306e1d574
guoliang <guoliang@hrtpayment.com>
2019-01-09 10:18:30 +0800

    :o: 构建测试

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import sys,os
 python import模块时， 是在sys.path里按顺序查找的。
 sys.path是一个列表，里面以字符串的形式存储了许多路径。
 '''
-sys.path.append(os.path.abspath("./src/org"))
+sys.path.append(os.path.abspath('src/org'))
 
 import org.lxg.test.module_test
 
