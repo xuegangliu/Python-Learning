b932b897fb5b91ee1dbad8e8c2974814b09743b5
guoliang <guoliang@hrtpayment.com>
2019-01-09 10:14:18 +0800

    :o: 构建测试

--- test.py
+++ test.py
@@ -2,6 +2,14 @@
 # coding=utf-8
 # author: xuegangliu
 
+import sys,os
+
+'''
+python import模块时， 是在sys.path里按顺序查找的。
+sys.path是一个列表，里面以字符串的形式存储了许多路径。
+'''
+sys.path.append(os.path.abspath("./src/org"))
+
 import org.lxg.test.module_test
 
 if __name__ == '__main__':
